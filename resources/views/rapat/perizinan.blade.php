522877d772691a743f3df0a47d2edc2861aa26ed
Styaaa <91019301+Styaa@users.noreply.github.com>
2025-04-07 09:35:56 +0800

    Update perizinan.blade.php

--- resources/views/rapat/perizinan.blade.php
+++ resources/views/rapat/perizinan.blade.php
@@ -14,7 +14,7 @@
                 <div class="border-0 mb-4">
                     <div
                         class="card-header py-3 no-bg bg-transparent d-flex align-items-center px-0 justify-content-between border-bottom flex-wrap">
-                        <h3 class="fw-bold mb-0">Candidate Members</h3>
+                        <h3 class="fw-bold mb-0">Daftar Izin Rapat</h3>
                         {{-- <div class="col-auto d-flex w-sm-100">
                             <button type="button" class="btn btn-dark btn-set-task w-sm-100" data-bs-toggle="modal"
                                 data-bs-target="#leaveadd"><i class="icofont-plus-circle me-2 fs-6"></i>Add Leave</button>
